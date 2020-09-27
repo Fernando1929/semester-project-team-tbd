a8bf0d133f995919b964289a83032503065f169b
Luis Fernando Quiles Ruiz <luis.quiles1@upr.edu>
2020-09-27 14:33:15 -0400

    Merge pull request #27 from uprm-inso-4101-2020-2021-S1/Yaritza-dev
    Yaritza dev: SignUp form done

--- team-tbd-project/src/Components/SignUpForm.jsx
+++ team-tbd-project/src/Components/SignUpForm.jsx
@@ -1,25 +1,89 @@
 import React, { Component } from "react";
-import { Form, Card } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
 
 import "../App.css";
 
 class SignUpForm extends Component {
+  emailIcon = (<i className="fab fa-user"></i>);
+
   render() {
     return (
-      <Card style={{ width: "18rem" }}>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-      </Card>
+      <Container>
+        <Row className="justify-content-center">
+          <Col sm={7}>
+            <Card style={{ borderRadius: "15px", marginTop: "3rem" }}>
+              <Card.Body>
+                <Card.Title>
+                  <h1
+                    className="text-center"
+                    style={{
+                      fontWeight: "600",
+                      fontFamily: "'Open Sans', sans-serif",
+                    }}
+                  >
+                    Sign Up
+                  </h1>
+                </Card.Title>
+                <Card.Text>
+                  <InputGroup style={{ marginBottom: "1rem" }}>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text>
+                        <i class="far fa-user"></i>
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl id="Username" placeholder="Username" />
+                  </InputGroup>
+                  <InputGroup style={{ marginBottom: "1rem" }}>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text>
+                        <i class="far fa-envelope"></i>
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl type="email" id="Email" placeholder="Email" />
+                  </InputGroup>
+                  <InputGroup style={{ marginBottom: "1rem" }}>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text>
+                        <i class="fas fa-lock"></i>
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                      type="password"
+                      id="Password"
+                      placeholder="Password"
+                    />
+                  </InputGroup>
+                  <InputGroup style={{ marginBottom: "1rem" }}>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text>
+                        <i class="fas fa-lock"></i>
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                      type="passwordReType"
+                      id="Re-enterPassword"
+                      placeholder="Re-enter Password"
+                    />
+                  </InputGroup>
+                </Card.Text>
+                <div className="text-center">
+                  <Button className="btn--primary" variant="primary">
+                    CREATE ACCOUNT
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
