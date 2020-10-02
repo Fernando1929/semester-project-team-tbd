6fa17b28aceeb559ec105f0f3cf0b295c81ff040
Yaritza M. García Chaparro <yaritza.garcia3@upr.edu>
2020-10-02 01:32:00 -0400

    Refactoring of components

--- team-tbd-project/src/Components/SignUpForm.jsx
+++ team-tbd-project/src/Components/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Container,
   Row,
@@ -11,81 +11,77 @@ import {
 
 import "../App.css";
 
-class SignUpForm extends Component {
-  emailIcon = (<i className="fab fa-user"></i>);
-
-  render() {
-    return (
-      <Container>
-        <Row className="justify-content-center">
-          <Col sm={7}>
-            <Card style={{ borderRadius: "15px", marginTop: "3rem" }}>
-              <Card.Body>
-                <Card.Title>
-                  <h1
-                    className="text-center"
-                    style={{
-                      fontWeight: "600",
-                      fontFamily: "'Open Sans', sans-serif",
-                    }}
-                  >
-                    Sign Up
-                  </h1>
-                </Card.Title>
-                <Card.Text>
-                  <InputGroup style={{ marginBottom: "1rem" }}>
-                    <InputGroup.Prepend>
-                      <InputGroup.Text>
-                        <i class="far fa-user"></i>
-                      </InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl id="Username" placeholder="Username" />
-                  </InputGroup>
-                  <InputGroup style={{ marginBottom: "1rem" }}>
-                    <InputGroup.Prepend>
-                      <InputGroup.Text>
-                        <i class="far fa-envelope"></i>
-                      </InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl type="email" id="Email" placeholder="Email" />
-                  </InputGroup>
-                  <InputGroup style={{ marginBottom: "1rem" }}>
-                    <InputGroup.Prepend>
-                      <InputGroup.Text>
-                        <i class="fas fa-lock"></i>
-                      </InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                      type="password"
-                      id="Password"
-                      placeholder="Password"
-                    />
-                  </InputGroup>
-                  <InputGroup style={{ marginBottom: "1rem" }}>
-                    <InputGroup.Prepend>
-                      <InputGroup.Text>
-                        <i class="fas fa-lock"></i>
-                      </InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                      type="passwordReType"
-                      id="Re-enterPassword"
-                      placeholder="Re-enter Password"
-                    />
-                  </InputGroup>
-                </Card.Text>
-                <div className="text-center">
-                  <Button className="btn--primary" variant="primary">
-                    CREATE ACCOUNT
-                  </Button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+function SignUpForm() {
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col sm={7}>
+          <Card style={{ borderRadius: "15px", marginTop: "3rem" }}>
+            <Card.Body>
+              <Card.Title>
+                <h1
+                  className="text-center"
+                  style={{
+                    fontWeight: "600",
+                    fontFamily: "'Open Sans', sans-serif",
+                  }}
+                >
+                  Sign Up
+                </h1>
+              </Card.Title>
+              <Card.Text>
+                <InputGroup style={{ marginBottom: "1rem" }}>
+                  <InputGroup.Prepend>
+                    <InputGroup.Text>
+                      <i class="far fa-user"></i>
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl id="Username" placeholder="Username" />
+                </InputGroup>
+                <InputGroup style={{ marginBottom: "1rem" }}>
+                  <InputGroup.Prepend>
+                    <InputGroup.Text>
+                      <i class="far fa-envelope"></i>
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl type="email" id="Email" placeholder="Email" />
+                </InputGroup>
+                <InputGroup style={{ marginBottom: "1rem" }}>
+                  <InputGroup.Prepend>
+                    <InputGroup.Text>
+                      <i class="fas fa-lock"></i>
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    type="password"
+                    id="Password"
+                    placeholder="Password"
+                  />
+                </InputGroup>
+                <InputGroup style={{ marginBottom: "1rem" }}>
+                  <InputGroup.Prepend>
+                    <InputGroup.Text>
+                      <i class="fas fa-lock"></i>
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    type="password"
+                    id="Re-enterPassword"
+                    placeholder="Re-enter Password"
+                  />
+                </InputGroup>
+              </Card.Text>
+              <div className="text-center">
+                <Button className="btn--primary" variant="primary">
+                  CREATE ACCOUNT
+                </Button>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default SignUpForm;
--- team-tbd-project/src/Components/SyncLinkNavbar.jsx
+++ team-tbd-project/src/Components/SyncLinkNavbar.jsx
@@ -1,96 +1,91 @@
-import React, { Component } from "react";
+import React from "react";
 import { Navbar, Nav, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import mainLogo from "../Images/synLogoNM.png";
 import "../App.css";
 
-class SyncLinkNavbar extends Component {
-  render() {
-    const navStyle = {
-      textDecoration: "none",
-    };
+function SyncLinkNavbar() {
+  const navStyle = {
+    textDecoration: "none",
+  };
 
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-        <Navbar.Brand href="/">
-          <h2
-            className="phoneDisplayNoText"
-            style={{
-              fontFamily: "'Roboto Mono', monospace",
-              alignItems: "center",
-              marginBottom: "0",
-            }}
-          >
-            <img
-              alt=""
-              src={mainLogo}
-              width="108"
-              height="62"
-              className="d-inline-block"
-              style={{ marginRight: "10px" }}
-            />
-            {""}
-            SYNCLINK
-          </h2>
-          <h2
-            className="phoneDisplayLogo"
-            style={{
-              fontFamily: "'Roboto Mono', monospace",
-              alignItems: "center",
-              marginBottom: "0",
-            }}
-          >
-            <img
-              alt=""
-              src={mainLogo}
-              width="108"
-              height="62"
-              className="d-inline-block"
-              style={{ marginRight: "10px" }}
-            />
-            {""}
-          </h2>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse
-          id="responsive-navbar-nav"
-          style={{ textAlign: "center" }}
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Navbar.Brand href="/">
+        <h2
+          className="phoneDisplayNoText"
+          style={{
+            fontFamily: "'Roboto Mono', monospace",
+            alignItems: "center",
+            marginBottom: "0",
+          }}
         >
-          <Nav className="mr-auto"></Nav>
-          <Nav>
-            <Nav.Link>
-              <Link to="/" style={navStyle}>
-                HOME
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Services" style={navStyle}>
-                SERVICES
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/AboutUs" style={navStyle}>
-                ABOUT US
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/ContactUs" style={navStyle}>
-                CONTACT US
-              </Link>
-            </Nav.Link>
+          <img
+            alt=""
+            src={mainLogo}
+            width="108"
+            height="62"
+            className="d-inline-block"
+            style={{ marginRight: "10px" }}
+          />
+          {""}
+          SYNCLINK
+        </h2>
+        <h2
+          className="phoneDisplayLogo"
+          style={{
+            fontFamily: "'Roboto Mono', monospace",
+            alignItems: "center",
+            marginBottom: "0",
+          }}
+        >
+          <img
+            alt=""
+            src={mainLogo}
+            width="108"
+            height="62"
+            className="d-inline-block"
+            style={{ marginRight: "10px" }}
+          />
+          {""}
+        </h2>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse
+        id="responsive-navbar-nav"
+        style={{ textAlign: "center" }}
+      >
+        <Nav className="mr-auto"></Nav>
+        <Nav>
+          <Nav.Link>
+            <Link to="/" style={navStyle}>
+              HOME
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/Services" style={navStyle}>
+              SERVICES
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/AboutUs" style={navStyle}>
+              ABOUT US
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/ContactUs" style={navStyle}>
+              CONTACT US
+            </Link>
+          </Nav.Link>
+          <Link to="/SignUp" style={{ textDecoration: "none", color: "white" }}>
             <Button className="btn--primary" variant="primary">
-              <Link
-                to="/SignUp"
-                style={{ textDecoration: "none", color: "white" }}
-              >
-                SIGN UP
-              </Link>
-            </Button>{" "}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+              SIGN UP
+            </Button>
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default SyncLinkNavbar;
