f09bb337e04c884514093ac8794ce9361d3ad5a6
Maria Vilanova <maria.vilanova@upr.edu>
2020-10-02 19:26:16 -0400

    Added hooks to sign up form (success!)

--- team-tbd-project/src/Components/SignUpForm.jsx
+++ team-tbd-project/src/Components/SignUpForm.jsx
@@ -13,6 +13,29 @@ import {
 import "../App.css";
 
 function SignUpForm() {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_rep, setPassword_rep] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if(password == password_rep){
+        const response = await SignUp.post("/signup", {
+          username: username,
+          email: email,
+          password: password
+        });
+        console.log(response);
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <Container>
       <Row className="justify-content-center">
@@ -37,7 +60,7 @@ function SignUpForm() {
                       <i class="far fa-user"></i>
                     </InputGroup.Text>
                   </InputGroup.Prepend>
-                  <FormControl id="Username" placeholder="Username" />
+                  <FormControl id="Username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
                 </InputGroup>
                 <InputGroup style={{ marginBottom: "1rem" }}>
                   <InputGroup.Prepend>
@@ -45,7 +68,7 @@ function SignUpForm() {
                       <i class="far fa-envelope"></i>
                     </InputGroup.Text>
                   </InputGroup.Prepend>
-                  <FormControl type="email" id="Email" placeholder="Email" />
+                  <FormControl type="email" id="Email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 </InputGroup>
                 <InputGroup style={{ marginBottom: "1rem" }}>
                   <InputGroup.Prepend>
@@ -57,6 +80,8 @@ function SignUpForm() {
                     type="password"
                     id="Password"
                     placeholder="Password"
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </InputGroup>
                 <InputGroup style={{ marginBottom: "1rem" }}>
@@ -69,11 +94,13 @@ function SignUpForm() {
                     type="password"
                     id="Re-enterPassword"
                     placeholder="Re-enter Password"
+                    value={password_rep} 
+                    onChange={(e) => setPassword_rep(e.target.value)}
                   />
                 </InputGroup>
               </Card.Text>
               <div className="text-center">
-                <Button className="btn--primary" variant="primary">
+                <Button onClick={(e) => handleSubmit(e)} type="submit" className="btn--primary" variant="primary">
                   CREATE ACCOUNT
                 </Button>
               </div>
