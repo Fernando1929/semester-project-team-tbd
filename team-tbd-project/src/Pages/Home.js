4ccbe9e121175654a272bb9d065bfb05330c9f29
YeranConcepcion <yeran.concepcion@upr.edu>
2020-10-21 16:48:49 -0400

    Stared fixing the home page, the background and new btn where added

--- team-tbd-project/src/Pages/Home.js
+++ team-tbd-project/src/Pages/Home.js
@@ -1,46 +1,69 @@
 import React from "react";
 import "../../src/App.css";
 import "./Home.css";
-import backgroundH from "../Images/HomeBackground.jpg";
-import { Button } from "react-bootstrap";
+import backgroundH from "../Images/newHomeBk.gif";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div
-      class="bk_Img"
-      style={{
-        backgroundImage: "url(" + backgroundH + ")",
-        backgroundSize: "cover",
-        height: "100vh",
-      }}
-    >
-      <ol className="homeButtons">
-        <Link to="/LogIn" style={{ textDecoration: "none", color: "white" }}>
-          <li>
-            <Button className="btn--secondary" variant="primary">
-              LOG IN
-            </Button>
-          </li>
-        </Link>
-        <Link to="/SignUp" style={{ textDecoration: "none", color: "white" }}>
-          <li>
-            <Button className="btn--secondary" variant="primary">
-              SIGN UP
-            </Button>
-          </li>
-        </Link>
-        <Link
-          to="/ContactUs"
-          style={{ textDecoration: "none", color: "white" }}
-        >
-          <li>
-            <Button className="btn--secondary" variant="primary">
-              HELP
-            </Button>
-          </li>
-        </Link>
-      </ol>
+    <div>
+      <Container
+        fluid
+        className="d-flex align-items-start "
+        style={{
+          backgroundImage: "url(" + backgroundH + ")",
+
+          backgroundSize: "100% auto",
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+        }}
+      >
+        <Row>
+          <Col
+            style={{
+              textAlign: "center",
+              marginTop: "35%",
+              marginLeft: "18%",
+            }}
+            sm
+          >
+            <h1 style={{ fontSize: "7vw" }}>SYNCLINK</h1>
+            <h3 style={{ fontSize: "2vw" }}>MEET AT THE PERFECT MOMENT</h3>
+            <div>
+              <Link
+                to="/SignUp"
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                }}
+              >
+                <Button
+                  className="btn--primary"
+                  variant="primary"
+                  style={{ fontSize: "2vw", marginBottom: "1rem" }}
+                >
+                  GET STARTED
+                </Button>
+              </Link>
+            </div>
+            <div>
+              <Link
+                to="/LogIn"
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                  fontSize: "2vw",
+                }}
+              >
+                <Button className="btn--secondary" style={{ fontSize: "2vw" }}>
+                  LOG IN
+                </Button>
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
