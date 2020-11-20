51e0e19ea0ce47a11315a004c251694a67c0d220
Yaritza M. García Chaparro <yaritza.garcia3@upr.edu>
2020-11-19 23:11:47 -0400

    Unnecessary stuff, deleted.

--- team-tbd-project/src/App/App.js
+++ team-tbd-project/src/App/App.js
@@ -4,12 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import SyncLinkNavbar from "../Components/SyncLinkNavbar";
 import Home from "../Pages/Home/Home";
-import Services from "../Pages/AboutUs/Services";
 import UserSchedule from "../Pages/User/UserSchedule";
 import SignUp from "../Pages/SignUp/SignUp";
 import ProfileInfo from "../Pages/SignUp/ProfileInfo";
-import AboutUs from "../Pages/AboutUs/AboutUs";
-import ContactUs from "../Pages/AboutUs/ContactUs";
 import LogIn from "../Pages/Login/LogIn";
 import LoginValidate from "../Pages/Login/LoginValidate";
 import Profile from "../Pages/User/Profile";
@@ -21,19 +18,16 @@ function App() {
   return (
     <>
       <Router>
-        {Auth.isUserAuthenticated()? <LoginNavbar/>:<SyncLinkNavbar />}
+        {Auth.isUserAuthenticated() ? <LoginNavbar /> : <SyncLinkNavbar />}
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/Services" component={Services} />
+          <Route path="/" exact component={Home} />
           <Route path="/SignUp" component={SignUp} />
           <Route path="/LogIn" component={LogIn} />
-          <Route path="/AboutUs" component={AboutUs} />
-          <Route path="/ContactUs" component={ContactUs} />
           <Route path="/ProfileInfo" component={ProfileInfo} />
           <Route path="/UserSchedule" component={UserSchedule} />
-          <Route path="/LoginValidate" component={LoginValidate}/>
-          <Route path="/Profile" component={Profile}/>
-          <Route path="/Teams" component={Teams}/>
+          <Route path="/LoginValidate" component={LoginValidate} />
+          <Route path="/Profile" component={Profile} />
+          <Route path="/Teams" component={Teams} />
         </Switch>
       </Router>
     </>
